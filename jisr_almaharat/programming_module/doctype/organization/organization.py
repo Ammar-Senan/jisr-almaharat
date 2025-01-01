cbca57459103230c468a848dca5b08501930b6c4
Ammar-Senan <22164081@su.edu.ye>
2025-01-01 17:13:33 +0300

    Add fields

--- jisr_almaharat/programming_module/doctype/organization/organization.py
+++ jisr_almaharat/programming_module/doctype/organization/organization.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, admin and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Organization(Document):
 	pass
+	
+
+	
+
