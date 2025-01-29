da2a48853adc831136777bf5279a9c16a44fee88
Barhoum122 <ibrahimanam122@gmail.com>
2025-01-29 16:38:56 +0000

    added the traning page and fixed the proprem url

--- jisr_almaharat/programming_module/web_form/application/application.py
+++ jisr_almaharat/programming_module/web_form/application/application.py
@@ -1,5 +1,23 @@
-import frappe
-
+import frappe # type: ignore
+# @frappe.whitelist(allow_guest=True)
 def get_context(context):
-	# do your magic here
-	pass
+	try:	
+			# this line to get Jobkind context from url
+			context.Job_kind =frappe.form_dict.get("Jobkind")
+			if context.Job_kind in ['Job','Training']:
+				context.Job_kind
+			# this line to get name context from url
+				context.detail = frappe.get_doc("Job",frappe.form_dict.docname)
+			# print(f"\n\n\n\n\n\n{context.Job_kind}\n\n\n\n\n")
+				return context
+			else:
+			# if the data in url not found or false go to page 404 
+				frappe.local.flags.redirect_location ='/404'
+				raise frappe.Redirect  
+
+	except Exception as e:
+					frappe.local.flags.redirect_location ='/404'
+					raise frappe.Redirect  
+	                
+
+				
--- jisr_almaharat/www/Home/jobDetails.py
+++ jisr_almaharat/www/Home/jobDetails.py
@@ -1,8 +1,8 @@
 import frappe # type: ignore
 def get_context(context):
-    # print(f"\n\n\n\n\n\n{context.detail}\n\n\n\n\n")
     try:
        context.detail = frappe.get_doc("Job",frappe.form_dict.docname)
+      #  print(f"\n\n\n\n\n\n{context.detail}\n\n\n\n\n")
     except Exception as e:   
        frappe.local.flags.redirect_location ='/404'
        raise frappe.Redirect       
