96cf1d6988d0bfb19a14fbc5f4c9c5b74efe98c2
Ammar-Senan <22164081@su.edu.ye>
2025-02-02 17:27:08 +0000

    Modyfiy: fit the web theme

--- jisr_almaharat/www/Home/index.py
+++ jisr_almaharat/www/Home/index.py
@@ -1,5 +1,6 @@
 import frappe # type: ignore
 def get_context(context):
     context.Job_info=frappe.get_all("Job",fields=["name","jop_title","aplication_deadline","organization_name","jop_type"])
+    context.Training_info=frappe.get_all("Training",fields=["name","training_title","aplication_deadline","organization_name","training_pattren"])
     return context
  
---
+++ jisr_almaharat/www/Home/more_training.py
@@ -0,0 +1,6 @@
+import frappe # type: ignore
+def get_context(context):
+    context.oline=frappe.get_all("Training",
+    fields=["name","training_title","about_training","aplication_deadline","training_post_date","organization_name","training_pattren","location"])
+    return context
+ 
