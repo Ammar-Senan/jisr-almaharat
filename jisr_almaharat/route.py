a8ab021ed886cf64a01a4dfcc2acc6896bb51bac
Barhoum122 <ibrahimanam122@gmail.com>
2025-02-04 19:43:44 +0000

    add login page and update main.css

--- jisr_almaharat/route.py
+++ jisr_almaharat/route.py
@@ -2,4 +2,5 @@ routes=[
     {'from_route':'/Home/jobDetails/<docname>','to_route':'Home/jobDetails'},
     {'from_route':'/application/<docname>/<Jobkind>','to_route':'application/new'},
     {'from_route':'/Home/training_details/<docname>','to_route':'Home/training_details'},
+    
 ]
