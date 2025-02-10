3a68a7dc736b9b695c58ea77066975460d1b774e
Ibrahim Esmail <ibrahmanam122@gmail.com>
2025-02-11 00:33:18 +0300

    Update appling.js

--- jisr_almaharat/programming_module/web_form/appling/appling.js
+++ jisr_almaharat/programming_module/web_form/appling/appling.js
@@ -1,24 +1,26 @@
-frappe.ready(function() {
-
-	frappe.web_form.set_value("kind", "{{Job_kind}}");
-
-	if ("{{Job_kind}}" == "Training") {
-		frappe.web_form.set_value("training_name", "{{detail.jop_title }}");
-	}else if ("{{Job_kind}}" == "Job") {
-		frappe.web_form.set_value("job_name", "{{detail.jop_title }}");
-	};
-	
-	//make job_name unhidden if kind is job and make training_name unhidden if kind is training in frappe web form
-	frappe.web_form.on('kind', (field, value) => {
-		if (value == "Training") {
-			frappe.web_form.set_df_property('job_name', 'hidden', 1);
-			frappe.web_form.set_df_property('training_name', 'hidden', 0);
-		}else if(value == "Job") {
-			frappe.web_form.set_df_property('job_name', 'hidden', 0);
-			frappe.web_form.set_df_property('training_name', 'hidden', 1);
-		};
+import frappe # type: ignore
+# @frappe.whitelist(allow_guest=True)
+def get_context(context):
+				try:	
+					# this line to get Jobkind context from url
+					context.Job_kind =frappe.form_dict.get("Jobkind")
+					# print(f"\n\n\n\n\n\n{context.Job_kind}\n\n\n\n\n")
+					if context.Job_kind in ['Job']:
+							context.Job_kind
+						   # this line to get name context from url 
+							context.detail = frappe.get_doc("Job",frappe.form_dict.docname)
+					elif context.Job_kind in ['Training']:
+							context.Job_kind
+							context.detail = frappe.get_doc("Training",frappe.form_dict.docname)
+							return context
+				
+					else:
+					# if the data in url not found or false go to page 404 
+							frappe.local.flags.redirect_location ='/404'
+							raise frappe.Redirect  
+							
+					
+				except Exception as e:
+							frappe.local.flags.redirect_location ='/404'
+							raise frappe.Redirect  
 		
-	});
-
-	
-})
