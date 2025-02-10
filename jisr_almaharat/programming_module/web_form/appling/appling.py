e716e16b9f6ff8b39684a18c581716a798a591b6
Ibrahim Esmail <ibrahmanam122@gmail.com>
2025-02-11 00:35:39 +0300

    Update appling.py

--- jisr_almaharat/programming_module/web_form/appling/appling.py
+++ jisr_almaharat/programming_module/web_form/appling/appling.py
@@ -1,24 +1,26 @@
 import frappe # type: ignore
 # @frappe.whitelist(allow_guest=True)
 def get_context(context):
-	try:	
-		# this line to get Jobkind context from url
-		context.Job_kind =frappe.form_dict.get("Jobkind")
-		#context._kind =frappe.form_dict.get("trainingkind")
-		if context.Job_kind in ['Job','Training']:
-				context.Job_kind
+				try:	
+					# this line to get Jobkind context from url
+					context.Job_kind =frappe.form_dict.get("Jobkind")
+					# print(f"\n\n\n\n\n\n{context.Job_kind}\n\n\n\n\n")
+					if context.Job_kind in ['Job']:
+							context.Job_kind
+						   # this line to get name context from url 
+							context.detail = frappe.get_doc("Job",frappe.form_dict.docname)
+					elif context.Job_kind in ['Training']:
+							context.Job_kind
+							context.detail = frappe.get_doc("Training",frappe.form_dict.docname)
+							return context
+				
+					else:
+					# if the data in url not found or false go to page 404 
+							frappe.local.flags.redirect_location ='/404'
+							raise frappe.Redirect  
+							
+					
+				except Exception as e:
+							frappe.local.flags.redirect_location ='/404'
+							raise frappe.Redirect  
 		
-			# this line to get name context from url
-				context.detail = frappe.get_doc("Job",frappe.form_dict.docname)
-			# context.detail = frappe.get_doc("Training",frappe.form_dict.docname)
-			# print(f"\n\n\n\n\n\n{context.Job_kind}\n\n\n\n\n")
-				return context
-		else:
-		# if the data in url not found or false go to page 404 
-				frappe.local.flags.redirect_location ='/404'
-				raise frappe.Redirect  
-
-	except Exception as e:
-				frappe.local.flags.redirect_location ='/404'
-				raise frappe.Redirect  
-			
