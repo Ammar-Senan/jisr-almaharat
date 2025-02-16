4a0a687b1257d1d9c07f617ab8b8e956da42a620
Ramzi thabit <r.y.thabit@gmail.com>
2025-02-16 15:49:16 +0000

    Add:add Filter job ctegories and some Emproevments

---
+++ jisr_almaharat/www/Home/more_categoris.py
@@ -0,0 +1,7 @@
+import frappe # type: ignore
+def get_context(context):
+    context.Categories=frappe.get_all("Job",
+    fields=["name","jop_title","aplication_deadline","organization_name","jop_type","job_categories","jop_location"])
+
+    return context
+ 
