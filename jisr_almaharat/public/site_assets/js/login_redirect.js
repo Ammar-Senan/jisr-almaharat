180630c5ca7989cf4031c28ff4b6ef44780027c8
Ammar-Senan <22164081@su.edu.ye>
2025-04-15 03:59:25 +0000

    Fix: logout, show message to org if not approved

--- jisr_almaharat/public/site_assets/js/login_redirect.js
+++
@@ -1,9 +0,0 @@
-frappe.ready(function() {
-    if (frappe.session.user !== "Guest") {
-        let redirectPath = localStorage.getItem("redirect_after_login");
-        if (redirectPath && redirectPath !== "/login") {
-            localStorage.removeItem("redirect_after_login");
-            window.location.href = redirectPath;
-        }
-    }
-});
